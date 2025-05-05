01389eb8bb5f66bd14fbf83d02833cadabbb6f8a
Taylor Kelley <taylorgkelley@outlook.com>
2025-05-05 15:04:38 -0500

    feat: some styling changes and routing config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import MainLayout from './pages/MainLayout';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: MainLayout,
+    children: [{ index: true, Component: Home }],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <header>
+        {/* Photo */}
+        <h1 className="text-4xl font-semibold">Hi, I'm Taylor Kelley</h1>
+        {/* title and contacts */}
+      </header>
+      <main>main content...</main>
+    </>
+  );
+}
+
+export default Home;
