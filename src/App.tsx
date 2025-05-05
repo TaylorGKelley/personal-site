89bd6abcb182876b699a9aea3bbbabc005d9adf8
Taylor Kelley <taylorgkelley@outlook.com>
2025-05-05 11:11:10 -0500

    Adding React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-4xl font-mono font-semibold">
-      Hi, I’m Taylor Kelley <img src="/images/wave-emoji.png" />
-    </h1>
-  );
+  return <main></main>;
 }
 
 export default App;
