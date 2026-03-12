9b24cacc46fd6be3e79c33d8dd93b4f8eae8c65d
TaylorGKelley <taylorgkelley04@gmail.com>
2026-03-12 11:47:55 -0500

    feat: adding links for headings

--- src/components/markdown/text-components.tsx
+++ src/components/markdown/text-components.tsx
@@ -1,27 +1,80 @@
+import Link from "next/link";
+
 export function h1({ children }: Readonly<React.PropsWithChildren>) {
-  return <h1 className="text-5xl font-semibold mt-4 mb-6">{children}</h1>;
+  return (
+    <Link href={`#${children}`}>
+      <h1
+        className="text-5xl font-semibold mt-4 mb-6 font-mono"
+        id={children?.toString()}
+      >
+        {children}
+      </h1>
+    </Link>
+  );
 }
 
 export function h2({ children }: Readonly<React.PropsWithChildren>) {
-  return <h2 className="text-4xl font-semibold mt-3 mb-5">{children}</h2>;
+  return (
+    <Link href={`#${children}`}>
+      <h2
+        className="text-4xl font-semibold mt-3 mb-5 font-mono"
+        id={children?.toString()}
+      >
+        {children}
+      </h2>
+    </Link>
+  );
 }
 
 export function h3({ children }: Readonly<React.PropsWithChildren>) {
-  return <h3 className="text-3xl font-medium mt-3 mb-4">{children}</h3>;
+  return (
+    <Link href={`#${children}`}>
+      <h3
+        className="text-3xl font-medium mt-3 mb-4 font-mono"
+        id={children?.toString()}
+      >
+        {children}
+      </h3>
+    </Link>
+  );
 }
 
 export function h4({ children }: Readonly<React.PropsWithChildren>) {
-  return <h4 className="text-2xl font-medium mt-2 mb-3">{children}</h4>;
+  return (
+    <Link href={`#${children}`}>
+      <h4
+        className="text-2xl font-medium mt-2 mb-3 font-mono"
+        id={children?.toString()}
+      >
+        {children}
+      </h4>
+    </Link>
+  );
 }
 
 export function h5({ children }: Readonly<React.PropsWithChildren>) {
-  return <h5 className="text-xl font-medium mt-2 mb-3">{children}</h5>;
+  return (
+    <Link href={`#${children}`}>
+      <h5
+        className="text-xl font-medium mt-2 mb-3 font-mono"
+        id={children?.toString()}
+      >
+        {children}
+      </h5>
+    </Link>
+  );
 }
 
 export function h6({ children }: Readonly<React.PropsWithChildren>) {
-  return <h6 className="text-lg mt-1 mb-2">{children}</h6>;
+  return (
+    <Link href={`#${children}`}>
+      <h6 className="text-lg mt-1 mb-2 font-mono" id={children?.toString()}>
+        {children}
+      </h6>
+      ;
+    </Link>
+  );
 }
-
 export function p({ children }: Readonly<React.PropsWithChildren>) {
   return <p className="text-base font-light mt-1 mb-2">{children}</p>;
 }
